8fc6497e0b7658d1b29bce36e77104107941215c
Daniela Meneses <dvargas@oildex.com>
2021-02-15 11:24:53 -0400

    Setup initial modules and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'patients', loadChildren: () => import('./patients/patients.module').then(m => m.PatientsModule) },
+  { path: 'foods', loadChildren: () => import('./foods/foods.module').then(m => m.FoodsModule) },
+  { path: '',   redirectTo: '/patients', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
